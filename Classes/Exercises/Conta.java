93a9bf2ed731c28a80c416a003bce9c8cf088b5b
Bruno <bomf@cesar.school>
2025-04-16 09:07:40 -0300

    add class exercise

---
+++ Classes/Exercises/Conta.java
@@ -0,0 +1,36 @@
+public class Conta {
+    private String numero;
+    private String nome;
+    private double saldo;
+
+    public Conta(String numero, String nome, double saldo) {
+        this.numero = numero;
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    
+}
